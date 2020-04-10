429f8d2c566f82383f4232487160ceb52c72dda8
Peter Dimitrov <prda2016@pomona.edu>
2020-04-09 22:26:23 -0700

    Updated map code to track country tags in tweets

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -64,6 +65,14 @@ with zipfile.ZipFile(args.input_path) as archive:
                         counter_lang[hashtag][lang] += 1
                     counter_lang['_all'][lang] += 1
 
+                for hashtag in hashtags:
+                    if tweet['place'] is not None:
+                        country = tweet['place']['country_code']
+                        if hashtag in text:
+                            counter_country[hashtag][country] += 1
+                        counter_country['_all'][country] += 1
+
+
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -75,4 +84,16 @@ output_path_lang = output_path_base+'.lang'
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+
+# open the outputfile
+try:
+    os.makedirs(args.output_folder)
+except FileExistsError:
+    pass
+output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
+
+output_path_country = output_path_base+'.country'
+print('saving',output_path_country)
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
 
